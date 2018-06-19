85fd63be0d38ce633193801e9ed9d289fac701ab
breiddy monterrey <breiddypreteroti@gmail.com>
2018-06-19 11:36:02 -0400

    1- Arreglo inicio de sesion (Diseño).

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -10,8 +10,10 @@
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
     <link rel="stylesheet" type="text/css" href="{{ asset('plugins/bootstrapV3.3/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('plugins/bootstrapV3.3/css/bootstrap.min.css') }}">
+
     <link rel="stylesheet" type="text/css" href="{{ asset('plugins/LTE/thema/font-awesome/css/font-awesome.min.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{asset('plugins/css/login3.css')}}">
+
     <link rel="stylesheet" type="text/css" href="{{asset('plugins/css/slick-team-slider.css')}}">
     <link rel="stylesheet" type="text/css" href="{{asset('plugins/css/style.css')}}">
     <link href="{{ asset('views/css/app.css') }}" rel="stylesheet">
@@ -44,58 +46,67 @@
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
                 <div class="panel panel-default">
-                    <div class="panel-heading">Iniciar sesión</div>
+                    <div class="panel-heading"><h3 class="text-primary"><b><center>Iniciar sesión</center></b></h3></div>
                     <div class="panel-body">
                         <form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
                             {{ csrf_field() }}
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <div class="row">
+                                    <div class="col-md-2"></div>
+                                        <div class="col-md-8">
+                                            <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                                                <label for="email" class="control-label">Correo </label><br>
 
-                            <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                                <label for="email" class="col-md-4 control-label">Correo </label>
-
-                                <div class="col-md-6">
-                                    <input id="email" type="email" class="form-control" name="email"
-                                           value="{{ old('email') }}" required autofocus>
-                                    @if ($errors->has('email'))
-                                        <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                    @endif
-                                </div>
-                            </div>
-
-                            <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                                <label for="password" class="col-md-4 control-label">Contraseña</label>
-
-                                <div class="col-md-6">
-                                    <input id="password" type="password" class="form-control" name="password" required>
-                                    @if ($errors->has('password'))
-                                        <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                    @endif
-                                </div>
-                            </div>
-
-                            <div class="form-group">
-                                <div class="col-md-6 col-md-offset-4">
-                                    <div class="checkbox">
-                                        <label>
-                                            <input type="checkbox"
-                                                   name="remember" {{ old('remember') ? 'checked' : '' }}> Recuerdame
-                                        </label>
+                                                    <input id="email" type="email" class="form-control" name="email" placeholder="Correo"
+                                                           value="{{ old('email') }}" required autofocus>
+                                                    @if ($errors->has('email'))
+                                                        <span class="help-block">
+                                                        <strong>{{ $errors->first('email') }}</strong>
+                                                    </span>
+                                                    @endif
+                                            </div>
+                                        </div>
+                                    <div class="col-md-2"></div>
                                     </div>
-                                </div>
-                            </div>
+                                    <div class="row">
+                                        <div class="col-md-2"></div>
+                                        <div class="col-md-8">
+                                            <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                                                <label for="password" class="control-label">Contraseña</label><br>
+                                                    <input type="password" class="form-control"  name="password" id="password" placeholder="Contraseña">
 
-                            <div class="form-group">
-                                <div class="col-md-8 col-md-offset-4">
-                                    <button type="submit" class="btn btn-primary">
-                                        Ingresar
-                                    </button>
 
-                                    <a class="btn btn-link" href="{{ url('/password/reset') }}">
-                                        Olvide contraseña
-                                    </a>
+                                                    @if ($errors->has('password'))
+                                                        <span class="help-block">
+                                                        <strong>{{ $errors->first('password') }}</strong>
+                                                    </span>
+                                                @endif
+                                            </div>
+                                        </div>
+                                    <div class="col-md-2"></div>
+                                    <div class="form-group">
+                                        <div class="col-md-6 col-md-offset-4">
+                                            <div class="checkbox">
+                                                <label>
+                                                    <input type="checkbox"
+                                                           name="remember" {{ old('remember') ? 'checked' : '' }}> Recuerdame
+                                                </label>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <div class="col-md-12 col-md-offset-4">
+                                            <button type="submit" class="btn btn-primary">
+                                                Ingresar
+                                            </button>
+
+                                            <a class="btn btn-link" href="{{ url('/password/reset') }}">
+                                                Olvide contraseña
+                                            </a>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </form>
