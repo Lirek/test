7cd809a1a401d8b3a609ce175e185e2a39b11a32
Mauricio Gonzalez <mauriciojose14@gmail.com>
2018-06-25 23:57:10 -0400

    Ajuste del template para el usuario final -> home.blade.php el cual contiene toda la informacion con respecto al main o contenido de la pantalla
     Adaptacion de al formato layouts en el cual se creo dentro de la carpeta layout el archivo app.blade.php el cual contiene el llamado a la estructura de la pagina incluyendo los extends a sus partes.
     Partes del layout se encuentran en la carpeta layout/partials y se encuentran dos archivos ->footer.blade (contiene el pie de pagina) y ->siderRigth.blade(contiene la barra ubicada del lado derecho de la pagina)
    
     De igual  manera s agrego enla carpeta public/assets todos los archivos usados para el estilo y la imagenes del template

---
+++ resources/views/layouts (old)/partials/buttom.blade.php
@@ -0,0 +1,42 @@
+    <div class="fixed-action-btn click-to-toggle" style="bottom: 45px; right: 24px;">
+      <a class="btn-floating btn-large blue waves-effect waves-light tooltipped" data-position="left" data-tooltip="Ver Contenido">
+        <i class="large material-icons">add</i>
+      </a>
+
+      <ul>
+        <li>
+          <a class="btn-floating blue"><i class="material-icons">video_library</i></a>
+          <a href="" class="btn-floating fab-tip">Series</a>
+        </li>
+
+        <li>
+          <a class="btn-floating blue"><i class="material-icons">book</i></a>
+          <a href="" class="btn-floating fab-tip">Libos</a>
+        </li>
+
+        <li>
+          <a class="btn-floating blue"><i class="material-icons">collections_bookmark</i></a>
+          <a href="" class="btn-floating fab-tip">Revistas</a>
+        </li>
+
+        <li>
+          <a class="btn-floating blue"><i class="material-icons">live_tv</i></a>
+          <a href="" class="btn-floating fab-tip">Televisoras</a>
+        </li>
+
+        <li>
+          <a class="btn-floating blue"><i class="material-icons">radio</i></a>
+          <a href="" class="btn-floating fab-tip">Radios</a>
+        </li>
+
+        <li>
+          <a class="btn-floating blue"><i class="material-icons">theaters</i></a>
+          <a href="" class="btn-floating fab-tip">Peliculas</a>
+        </li>
+
+        <li>
+          <a class="btn-floating blue"><i class="material-icons">music_note</i></a>
+          <a href="{{url('MusicContent')}}" class="btn-floating fab-tip">Musica</a>
+        </li>
+      </ul>
+    </div>
