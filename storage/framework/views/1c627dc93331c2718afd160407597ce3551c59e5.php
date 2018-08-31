e22a5adfd80f4d66ee6f3829d16850e20da0f1c5
unknown <caknekto@gmail>
2018-08-31 03:48:22 +0200

    sistema optimizado a ver

--- storage/framework/views/1c627dc93331c2718afd160407597ce3551c59e5.php
+++
@@ -1,8 +0,0 @@
-<?php $__env->startSection('main'); ?>
-
-<?php $__env->stopSection(); ?>
-
-<?php $__env->startSection('js'); ?>
-
-<?php $__env->stopSection(); ?>
-<?php echo $__env->make('promoter.layouts.app', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
