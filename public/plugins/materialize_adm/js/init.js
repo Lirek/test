76001a83c257776440dccb4cf7cefc07681baa6b
Beyker Monterrey <beykerpreteroti07@gmail.com>
2018-11-09 09:29:28 -0400

    Bmonterrey: New layouts admin proveedores
    # Conflicts:
    #	.idea/workspace.xml
    #	storage/logs/laravel.log

---
+++ public/plugins/materialize_adm/js/init.js
@@ -0,0 +1,16 @@
+(function($){
+ 
+$(document).ready(function(){
+        // Initialize collapsible (uncomment the lines below if you use the dropdown variation)
+        $('.sidenav').sidenav();  //inicializa el boton para mostara el menu en pantallas small
+        $('.collapsible').collapsible();
+        $('.dropdown-trigger').dropdown();
+        $('.button-collapse').sideNav();
+
+    });
+
+})(jQuery); // end of jQuery name space
+
+
+
+
