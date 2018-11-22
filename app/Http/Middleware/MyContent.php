a8b4ee1f15c264633153539cfa73314ed4c2df74
Marcos Perez Jimenez <caknekto@gmail.com>
2018-11-21 22:59:47 -0400

    cambios en el login y configuraciones iniciales en la seguridad del contenido

---
+++ app/Http/Middleware/MyContent.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+use Auth;
+use User;
+use Transactions;
+
+class MyContent
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        
+        return $next($request);
+    }
+}
