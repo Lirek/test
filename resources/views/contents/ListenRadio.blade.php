571fb4c84bba503db7e1d77bba665740924967f7
Beyker Monterrey <beykerpreteroti07@gmail.com>
2019-01-08 11:45:18 -0400

    ajustes botones redes radio

--- resources/views/contents/ListenRadio.blade.php
+++ resources/views/contents/ListenRadio.blade.php
@@ -77,6 +77,18 @@
                         </div>
                     </div>
                 </div>
+
+                <div class="fixed-action-btn click-to-toggle direction-top">
+                    <a class="btn-floating btn-large waves-effect waves-light green">
+                        <i class="mdi mdi-forum-outline"></i>
+                    </a>
+                    <ul>
+                        <li><a href="{{$radios->facebook}}"  target="_blank" class="btn-floating blue darken-4"><i class="mdi mdi-facebook"></i></a></li>
+                        <li><a href="{{$radios->google}}"    target="_blank"  class="btn-floating red accent-4"><i class="mdi mdi-youtube"></i></a></li>
+                        <li><a href="{{$radios->twitter}}"   target="_blank" class="btn-floating blue lighten-2"><i class="mdi mdi-twitter"></i></a></li>
+                        <li><a href="{{$radios->instagram}}" target="_blank" class="btn-floating purple-gradient"><i class="mdi mdi-instagram"></i></a></li>
+                    </ul>
+                </div>
             @endforeach
         </div>
 
@@ -111,17 +123,7 @@
 
 
 
-    <div class="fixed-action-btn click-to-toggle direction-top">
-        <a class="btn-floating btn-large waves-effect waves-light green">
-            <i class="mdi mdi-forum-outline"></i>
-        </a>
-        <ul>
-            <li><a href="{{$radios->facebook}}"  target="_blank" class="btn-floating blue darken-4"><i class="mdi mdi-facebook"></i></a></li>
-            <li><a href="{{$radios->google}}"    target="_blank"  class="btn-floating red accent-4"><i class="mdi mdi-youtube"></i></a></li>
-            <li><a href="{{$radios->twitter}}"   target="_blank" class="btn-floating blue lighten-2"><i class="mdi mdi-twitter"></i></a></li>
-            <li><a href="{{$radios->instagram}}" target="_blank" class="btn-floating purple-gradient"><i class="mdi mdi-instagram"></i></a></li>
-        </ul>
-    </div>
+
 
 @endsection
 
