0e56bb4941101bbf314dc332f6a3193819572481
kevin <kevinjuvenal19@gmail.com>
2019-03-21 14:22:10 -0400

    Poner el trailer de pelicula más a la izquierda

--- resources/views/contents/PlayMovie.blade.php
+++ resources/views/contents/PlayMovie.blade.php
@@ -41,7 +41,7 @@
                 
 
                 <div id="test-swipe-1" class="col s12 ">
-                  <div class="col s12 m10 offset-m1" style="color: black">
+                  <div class="col s12" style="color: black">
     
                    <?php
                         $url = $m->trailer_url;
