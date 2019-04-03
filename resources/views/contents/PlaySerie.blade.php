9cf1c572a282f04b942de47a8265715da05ccdcb
kevin <kevinjuvenal19@gmail.com>
2019-04-03 15:17:43 -0400

    Aumentar el tamaño de la portada en pelicula y serie .

--- resources/views/contents/PlaySerie.blade.php
+++ resources/views/contents/PlaySerie.blade.php
@@ -65,7 +65,7 @@
             <div class="row ">
               <br>
               <div class="col s12 m3">
-                <img src="../movie/poster/{{$s->img_poster}}" width="100%" height="520px">
+                <img src="../movie/poster/{{$s->img_poster}}" width="100%" height="350px">
              </div>
 
               <div class="col m8 s12">
