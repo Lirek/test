261990c1c810f36ce4b0bcff48ad370f3ad91d78
kevin <kevinjuvenal19@gmail.com>
2019-06-11 11:14:17 -0400

    Cambio de imagen de fondo backend

--- resources/views/promoter/auth/login.blade.php
+++ resources/views/promoter/auth/login.blade.php
@@ -39,7 +39,7 @@
   <body>
   <div class="had-container">
        <div class="parallax-container logueo">
-        <div class="parallax"><img src="{{asset('sistem_images/promoter_assing.png')}}"></div>
+        <div class="parallax"><img style="width:100%; height:100% "src="{{asset('sistem_images/promoter_assing-2.jpg')}}"></div>
         <div class="row"><br>
           <div class="col m8 s8 offset-m2 offset-s2 center">              
             <div class="row login">
